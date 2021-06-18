3db195f2f08fbea20d29a67e5063d31b52b88eae
JorshuaG <joshbgearheart@gmail.com>
2021-06-17 22:07:08 -1000

    finished population task

--- index.js
+++ index.js
@@ -133,8 +133,11 @@ function lowerCaseNames(array) {
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-function lowPopulationAnimals(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lowPopulationAnimals(array) {
+  const animalPopulations = array.filter(function (item) {
+    return item.population < 5;
+  });
+  return animalPopulations;
 }
 
 /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
@@ -143,8 +146,11 @@ function lowPopulationAnimals(/*Your Code Here*/) {
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-function USApop(/*Your Code Here*/) {
-  /*Your Code Here*/
+function USApop(array) {
+  const totalPopulation = array.reduce(function (accumulator, item) {
+    return accumulator + item.population;
+  }, 0);
+  return totalPopulation;
 }
 
 // 🦁🦁🦁 Callbacks 🦁🦁🦁
